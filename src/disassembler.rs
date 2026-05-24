023733d980f06ec1234fb879087c13da8a7b3eb9
zer0x64 <dugre.philippe@hotmail.com>
2026-05-24 09:43:08 -0400

    switch to capstone for consistency

--- src/disassembler.rs
+++ src/disassembler.rs
@@ -1,4 +1,4 @@
-use iced_x86::{Decoder, DecoderOptions, Formatter, GasFormatter, IntelFormatter, Instruction};
+use capstone::prelude::*;
 
 #[derive(Debug, Clone)]
 pub struct DisassembledInstruction {
@@ -13,52 +13,39 @@ pub struct DisassembledInstruction {
 /// # Arguments
 /// * `code` - Raw binary machine code.
 /// * `base_address` - The base instruction pointer address.
-/// * `att_syntax` - If true, uses AT&T syntax (GasFormatter). If false, uses Intel syntax.
+/// * `att_syntax` - If true, uses AT&T syntax. If false, uses Intel syntax.
 pub fn disassemble_code(code: &[u8], base_address: u64, att_syntax: bool) -> Vec<DisassembledInstruction> {
     if code.is_empty() {
         return Vec::new();
     }
 
-    let mut decoder = Decoder::new(64, code, DecoderOptions::NONE);
-    decoder.set_ip(base_address);
+    let mut builder = Capstone::new()
+        .x86()
+        .mode(arch::x86::ArchMode::Mode64);
+
+    if att_syntax {
+        builder = builder.syntax(arch::x86::ArchSyntax::Att);
+    } else {
+        builder = builder.syntax(arch::x86::ArchSyntax::Intel);
+    }
+
+    let cs = match builder.build() {
+        Ok(cs) => cs,
+        Err(_) => return Vec::new(),
+    };
+
+    let insns = match cs.disasm_all(code, base_address) {
+        Ok(insns) => insns,
+        Err(_) => return Vec::new(),
+    };
 
     let mut instructions = Vec::new();
-    let mut instruction = Instruction::default();
-
-    // Create formatter conditionally
-    let mut intel_formatter = if !att_syntax { Some(IntelFormatter::new()) } else { None };
-    let mut gas_formatter = if att_syntax { Some(GasFormatter::new()) } else { None };
-
-    while decoder.can_decode() {
-        let offset = decoder.position();
-        decoder.decode_out(&mut instruction);
-        let len = instruction.len();
-
-        let inst_bytes = if offset + len <= code.len() {
-            code[offset..(offset + len)].to_vec()
-        } else {
-            code[offset..].to_vec()
-        };
-
-        let mut formatted = String::new();
-        if att_syntax {
-            gas_formatter.as_mut().unwrap().format(&instruction, &mut formatted);
-        } else {
-            intel_formatter.as_mut().unwrap().format(&instruction, &mut formatted);
-        }
-
-        // Format to split mnemonic and operands
-        let parts: Vec<&str> = formatted.splitn(2, ' ').collect();
-        let mnemonic = parts[0].to_string();
-        let op_str = if parts.len() > 1 {
-            parts[1].to_string()
-        } else {
-            String::new()
-        };
-
+    for insn in insns.as_ref() {
+        let mnemonic = insn.mnemonic().unwrap_or("").to_string();
+        let op_str = insn.op_str().unwrap_or("").to_string();
         instructions.push(DisassembledInstruction {
-            address: instruction.ip(),
-            bytes: inst_bytes,
+            address: insn.address(),
+            bytes: insn.bytes().to_vec(),
             mnemonic,
             op_str,
         });
@@ -77,7 +64,7 @@ mod tests {
         let insts = disassemble_code(&bytes, 0x10000000, false);
         assert_eq!(insts.len(), 1);
         assert_eq!(insts[0].mnemonic, "mov");
-        assert_eq!(insts[0].op_str, "rax,1");
+        assert_eq!(insts[0].op_str, "rax, 1");
     }
 
     #[test]
@@ -85,8 +72,8 @@ mod tests {
         let bytes = vec![0x48, 0xC7, 0xC0, 0x01, 0x00, 0x00, 0x00];
         let insts = disassemble_code(&bytes, 0x10000000, true);
         assert_eq!(insts.len(), 1);
-        assert_eq!(insts[0].mnemonic, "mov");
-        assert_eq!(insts[0].op_str, "$1,%rax");
+        assert_eq!(insts[0].mnemonic, "movq");
+        assert_eq!(insts[0].op_str, "$1, %rax");
     }
 }
 
